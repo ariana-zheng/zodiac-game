5a5c35c8f7fe9e8cdcfa2c3e32ebee284f7d3f14
342354727 <342354727@C-9Q1MHK2.tdsb.on.ca>
2025-06-09 10:19:59 -0400

    added background images, initialized character objects, working with collision detections

--- src/zodiacgame/MySketch.java
+++ src/zodiacgame/MySketch.java
@@ -11,49 +11,84 @@ package zodiacgame;
  * @author 342354727
  */
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class MySketch extends PApplet {
 
-    private Character user1, user2;
-    private boolean showInfo = false;
+    private Character rat, ox, tiger, rabbit, dragon, snake, horse, lamb, monkey, chicken, dog, pig;
+    private PImage image;    
+    
 
     public void settings() {
         //sets the size of the window
-        size(400, 400);
+        size(600, 400);
     }
 
     public void setup() {
         //sets the background colour using R,G,B (https://rgbcolorpicker.com/)
         background(100, 100, 100);
-        textSize(20);
-        user1 = new Person(this, 0, 0, "Ariana", 17, "images/person.png");
-        user2 = new Person(this, 100, 200, "Noohn", 17, "images/person.png");
+        textSize(20);//set text size
+        
+        //initialize every character
+        rat = new Character(this, 100, 100, "User", 0, "images/rat.png"); 
+        ox = new Character(this, 200, 100, "User", 0, "images/ox.png"); 
+        tiger = new Character(this, 300, 100, "User", 0, "images/tiger.png"); 
+        rabbit = new Character(this, 400, 100, "User", 0, "images/rabbit.png"); 
+        dragon = new Character(this, 100, 200, "User", 0, "images/dragon.png"); 
+        snake = new Character(this, 200, 200, "User", 0, "images/snake.png"); 
+        horse = new Character(this, 300, 200, "User", 0, "images/horse.png"); 
+        lamb = new Character(this, 400, 200, "User", 0, "images/lamb.png"); 
+        monkey = new Character(this, 100, 300, "User", 0, "images/monkey.png"); 
+        chicken = new Character(this, 200, 300, "User", 0, "images/chicken.png"); 
+        dog = new Character(this, 300, 300, "User", 0, "images/dog.png"); 
+        pig = new Character(this, 400, 300, "User", 0, "images/pig.png"); 
+        
+        this.image = loadImage("images/river.png"); //sets background image to river
 
     }
 
     public void draw() {
-        background(255);
-        person2.draw();
+        //draw background
+        image(image, 0, 0);
+       
+        //draw background
+       // background(255); 
+        
+        //draw every character
+        rat.draw();
+        ox.draw();
+        tiger.draw();
+        rabbit.draw();
+        dragon.draw();
+        snake.draw();
+        horse.draw();
+        lamb.draw();
+        monkey.draw();
+        chicken.draw();
+        dog.draw();
+        pig.draw();
+        
+        
 
         if (keyPressed) {
             if (keyCode == LEFT) {
-                person1.move(-5, 0);
+                rat.move(-5, 0);
             } else if (keyCode == RIGHT) {
-                person1.move(5, 0);
+                rat.move(5, 0);
             } else if (keyCode == UP) {
-                person1.move(0, -5);
+                rat.move(0, -5);
             } else if (keyCode == DOWN) {
-                person1.move(0, 5);
+                rat.move(0, 5);
             }
         }
-        person1.draw();
+   
         drawCollisions();
     }
 
     public void drawCollisions() {
-        if (person1.isCollidingWith(person2)) {
+        if (rat.isCollidingWith(ox)) {
             fill(255, 0, 0);
-            this.text("ouch", person2.x, person2.y);
+            this.text("Boost Found!", 250, 50);
         }
     }
 
